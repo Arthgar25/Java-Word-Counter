51efc2ce11c38dcb2f1ad319841e1133ea7f18af
Arthgar25 <desarthgar@gmail.com>
2026-01-18 12:18:17 -0800

    pulled code from an old assignment, corrected mistakes made

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         String fileName = "jobRequirements.txt";
+        HashMap<String, Integer> frequency = new HashMap<>();
+
         try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
-            Striung line;
+            String line;
 
             while((line = br.readLine()) != null){
-                System.out.println(line);
+                String[] words = line.split("//W+");
+
+                for(String word : words){
+                    if(word.isEmpty())continue;
+
+                    word = word.toLowerCase();
+                    frequency.put(word, frequency.getOrDefault(word, 0) + 1);
+                }
             }
-        } catch (IOException e){
-            System.err.println(e);
+        } catch(Exception e){
+            System.out.println(e.getMessage());
         }
+        frequency.forEach((key, value) -> System.out.println(key + ": " + value));
     }
 }
